8de485761e1e3bbdcf74be1a50ad54f28503db10
roandr1970 <ambal007a@yandex.ru>
2021-08-10 20:09:21 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer_amount = 500_000_000;
+        int total = balance + transfer_amount;
+        System.out.println(total);
+    }
+}
